7555ac5e38d0ee77ee58a602198f0ca767c33acc
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-06-25 15:14:49 +0300

    +PlayerCharacter

---
+++ LastDungeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        animator.SetFloat("Speed", Mathf.Abs(moveVelocity.x));
+
+        if (moveVelocity.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetBool("Strike", true);
+        } else if (Input.GetKeyUp(KeyCode.F))
+        {
+            animator.SetBool("Strike", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
